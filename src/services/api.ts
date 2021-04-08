75ced4f92c5ca78ce831926fb70d193c6b9a7fd9
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-08 15:39:57 -0300

    move api url to .env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://imovel-api.herokuapp.com'
+  baseURL: process.env.NEXT_PUBLIC_API_URL
 })
 
-const STORAGE_URL = 'https://res.cloudinary.com/imovel/image/upload/v1602331961'
+const STORAGE_URL = process.env.NEXT_PUBLIC_API_STORAGE
 
 export default api
 
