a86242d3a6c6e04ea02f7ac05fd0c134d197ca28
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-04-16 09:36:20 -0300

    start home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
-import { Header, FilterModal } from '../components'
+import { Header, FilterModal, Input, Button } from '../components'
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons'
+
+import Logo from '../assets/logo-black.png'
 
 const Home: React.FC = () => {
-  const [filterOpen, setFilterOpen] = useState(true)
+  const [filterOpen, setFilterOpen] = useState(false)
+  const [searchText, setSearchText] = useState('')
 
   return (
     <div>
@@ -11,7 +16,7 @@ const Home: React.FC = () => {
         <title>IMovel - Nunca foi tão fácil alugar um imóvel</title>
       </Head>
 
-      <main>
+      <main className="home-page">
         <Header />
 
         <FilterModal
@@ -19,6 +24,33 @@ const Home: React.FC = () => {
           onToggle={() => setFilterOpen(false)}
           applyFilter={options => console.log(options)}
         />
+
+        <form className="home-form">
+          <img src={Logo} alt="IMovel" />
+          <h1>Encontre o imóvel ideal para você!</h1>
+          <Input
+            // icon={faSearch}
+            placeholder="Pesquise por localidade..."
+            value={searchText}
+            onChange={e => setSearchText(e.target.value)}
+          />
+          <div className="filter">
+            <div className="filter-location">
+              <p className="filter-title">Imóveis para alugar</p>
+              <p className="filter-city">Santa Rita do Sapucaí, MG</p>
+            </div>
+            <Button
+              className="filter-btn"
+              type="button"
+              onClick={() => {
+                setFilterOpen(true)
+              }}
+            >
+              <Icon id="icon" icon={faFilter} />
+              Filtrar
+            </Button>
+          </div>
+        </form>
       </main>
     </div>
   )
