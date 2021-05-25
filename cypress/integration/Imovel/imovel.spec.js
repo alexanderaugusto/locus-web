bc4308ddf04285fa2b4afbde7b52f37f05b58390
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-05-25 10:08:53 -0300

    adicionando testes

--- cypress/integration/Imovel/imovel.spec.js
+++ cypress/integration/Imovel/imovel.spec.js
@@ -10,6 +10,14 @@ describe('Caso de Teste: 1', () => {
     cy.get('h1').should('contain.text', 'Encontre o imóvel ideal para você!')
   })
 
+  it('Cenario: Navegar até a tela de Detalhes do primeiro imóvel e verificar o título', () => {
+    cy.get(':nth-child(1) > .info-container > .more-details').click()
+    cy.get('.title').should(
+      'contain.text',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    )
+  })
+
   it('Cenario: Navegar até a tela de login e ao entrar com dados inválidos, não realizar o login', () => {
     cy.get('.dropdown-toggle > #icon > path').click()
     cy.get('.menu-not-signed > a').click()
@@ -33,13 +41,41 @@ describe('Caso de Teste: 1', () => {
     cy.get('.user-info > div > p').should('contain.text', 'Vanessa')
   })
 
-  it('Cenario de Teste: 4', () => {
-    // TODO os seus comandos de teste
+  it('Cenario: Realizar login e navegar até a tela Minha Conta e alterar o nome do usuário', () => {
+    cy.get('.dropdown-toggle > #icon > path').click()
+    cy.get('.menu-not-signed > a').click()
+    cy.get(':nth-child(2) > .input-container > input').type(
+      'vanessa@imovel.com'
+    )
+    cy.get(':nth-child(3) > .input-container > input').type('12345678')
+    cy.get('.button-container').click()
+
+    cy.get('.dropdown-toggle > img').click()
+    cy.get('ul > :nth-child(1) > a').click()
+
+    cy.get(':nth-child(1) > .input-container > input')
+      .focus()
+      .clear()
+      .type('Vanessa')
+    cy.get('form > .button-container').click()
+
+    cy.get('.user-info > p').should('contain.text', 'Vanessa')
   })
-  it('Cenario de Teste: 5', () => {
-    // TODO os seus comandos de teste
-  })
-  it('Cenario de Teste: 6', () => {
-    // TODO os seus comandos de teste
+
+  it('Cenario: Realizar Login, navegar ate a Tela Anunciar e entrar na tela de Cadastro de Imóveis', () => {
+    cy.get('.dropdown-toggle > #icon > path').click()
+    cy.get('.menu-not-signed > a').click()
+    cy.get(':nth-child(2) > .input-container > input').type(
+      'vanessa@imovel.com'
+    )
+    cy.get(':nth-child(3) > .input-container > input').type('12345678')
+    cy.get('.button-container').click()
+
+    cy.get('.dropdown-toggle > img').click()
+    cy.get('ul > :nth-child(3) > a').click()
+    cy.get('.title > h1').contains('Anunciar')
+
+    cy.get('.button-container').click()
+    cy.get('h1').should('contain.text', 'É rápido, simples e gratuito!')
   })
 })
